108b1ff80728840d93ffa2e8bc54aabf4bf8f050
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2022-11-14 05:58:01 +0900

    feat : HomeBanner 컴포넌트 생성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,18 @@
 import styled from 'styled-components';
+import HomeBanner from 'components/home/HomeBanner';
 
 function Home() {
-  const homeBannerImgList: string[] = [];
-
   return (
     <StHomeContainer>
-      <StHomebannerWrapper>
-        {homeBannerImgList.map((img, idx) => (
-          <StHomeBannerImg key={idx} />
-        ))}
-      </StHomebannerWrapper>
+      <HomeBanner />
     </StHomeContainer>
   );
 }
 
 export default Home;
 
-const StHomeContainer = styled.section``;
-
-const StHomebannerWrapper = styled.div``;
-
-const StHomeBannerImg = styled.img``;
+const StHomeContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
