53df5dfe5b9f5fc560cf21040a2550ff3c9a0ffc
김형겸 <97586683+Brokyeom@users.noreply.github.com>
2022-11-14 05:46:01 +0900

    feat : 홈 화면 기초 컴포넌트 구성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import styled from 'styled-components';
 
 function Home() {
-  return <div>홈화면이에요!</div>;
+  const homeBannerImgList: string[] = [];
+
+  return (
+    <StHomeContainer>
+      <StHomebannerWrapper>
+        {homeBannerImgList.map((img, idx) => (
+          <StHomeBannerImg key={idx} />
+        ))}
+      </StHomebannerWrapper>
+    </StHomeContainer>
+  );
 }
 
 export default Home;
+
+const StHomeContainer = styled.section``;
+
+const StHomebannerWrapper = styled.div``;
+
+const StHomeBannerImg = styled.img``;
