adbada8d06470945a43d18af86dbffec9a3d32c8
choichoijin <cuj10594@yonsei.ac.kr>
2022-11-16 20:09:17 +0900

    assets: 아이콘 추가

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as iconArrowBig } from './icon_arrow_big.svg';
+export { default as iconArrowSmall } from './icon_arrow_small.svg';
+export { default as iconHomeCategory } from './icon_home_category.svg';
