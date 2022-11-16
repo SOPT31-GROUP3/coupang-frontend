83749b26bf92401299dc1b2b79b7e2aa85d71373
parkhyeonji <iamphj3@naver.com>
2022-11-16 12:01:20 +0900

    init: nav.tsx 파일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import theme from 'styles/theme';
 import GlobalStyles from './styles/GlobalStyle';
 import Router from 'components/common/Router';
 import Header from 'components/common/Header';
+import Nav from 'components/common/Nav';
 import Footer from 'components/common/Footer';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <GlobalStyles />
       <Header />
       <Router />
+      <Nav />
       <Footer />
     </ThemeProvider>
   );
---
+++ src/components/common/Nav.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Nav() {
+  return <div>하단탭이에여!</div>;
+}
+
+export default Nav;
