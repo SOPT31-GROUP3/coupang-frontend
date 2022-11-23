cc34a9d32279a62a0be6ecea6a3ffd5722968ba2
choichoijin <cuj10594@yonsei.ac.kr>
2022-11-24 02:02:19 +0900

    design: 카테고리 메뉴 영역 구현

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,2 +1,3 @@
-export { default as imgLogo } from './img_logo.svg';
 export { default as iconBasketMain } from './icon_basket_main.svg';
+export { default as imgLogo } from './img_logo.svg';
+export { default as imgMenuTopItem } from './img_menu_top_item.svg';
--- src/components/category/Menu.tsx
+++ src/components/category/Menu.tsx
@@ -1,21 +1,35 @@
-import { iconArrowBig, iconArrowBigLast, iconHomeCategory } from 'assets/icons';
+import { iconArrowSmall } from 'assets/icons';
+import { imgMenuTopItem } from 'assets/images';
 import styled from 'styled-components';
 
 function Menu() {
+  const menuCategoryList = [
+    '전체',
+    '기초/기능성케어',
+    '메이크업/클렌징',
+    '헤어/바디',
+    '향수/미용',
+    '남성화장품',
+    '네일케어',
+    '',
+    '',
+  ];
   return (
     <>
       <MenuTopWrapper>
-        <MenuTop>
-          <MenuTopItem>
-            <StIconHomeCategory src={iconHomeCategory} />
-          </MenuTopItem>
-          <StIconArrowBig src={iconArrowBig} />
-          <MenuTopItem>쇼핑</MenuTopItem>
-          <StIconArrowBig src={iconArrowBigLast} />
-          <MenuTopItem>뷰티</MenuTopItem>
-        </MenuTop>
+        <StMenuTop src={imgMenuTopItem} alt='카테고리 메뉴' />
       </MenuTopWrapper>
-      <MenuCategory></MenuCategory>
+
+      <MenuCategoryWrapper>
+        {menuCategoryList.map((menuCategory) => {
+          return (
+            <MenuCategory key={menuCategory}>
+              <p>{menuCategory}</p>
+              <img src={iconArrowSmall} alt='이동하기' />
+            </MenuCategory>
+          );
+        })}
+      </MenuCategoryWrapper>
     </>
   );
 }
@@ -29,47 +43,34 @@ const MenuTopWrapper = styled.section`
   align-items: center;
 `;
 
-const MenuTop = styled.div`
+const StMenuTop = styled.img``;
+
+const MenuCategoryWrapper = styled.section`
   display: flex;
-  align-items: center;
-  width: 35.5rem;
-  height: 2.8rem;
-  border: 0.1rem solid ${(props) => props.theme.color.gray500};
-  border-radius: 0.3rem;
-  overflow: hidden;
+  flex-wrap: wrap;
+  border-left: 0.1rem solid ${(props) => props.theme.color.gray300};
+  border-top: 0.1rem solid ${(props) => props.theme.color.gray300};
 `;
 
-const MenuTopItem = styled.div`
-  height: 100%;
+const MenuCategory = styled.div`
   display: flex;
-  align-items: center;
-  padding: 0 0.6rem 0 0.8rem;
-  font-size: 1.1rem;
+  position: relative;
+  width: 12.46rem;
+  height: 3.8rem;
+  font-size: 1.2rem;
+  font-weight: ${(props) => props.theme.fontWeight.medium};
+  border-right: 0.1rem solid ${(props) => props.theme.color.gray300};
+  border-bottom: 0.1rem solid ${(props) => props.theme.color.gray300};
 
-  &:nth-of-type(2) {
-    width: 3.3rem;
-    letter-spacing: -0.02em;
-    color: ${(props) => props.theme.color.gray700};
-    font-weight: ${(props) => props.theme.fontWeight.medium};
+  & > p {
+    margin: 1.2rem 0 0 0.8rem;
   }
 
-  &:last-of-type {
-    padding-right: 0;
-    width: 26rem;
-    color: ${(props) => props.theme.color.gray800};
-    font-weight: ${(props) => props.theme.fontWeight.semiBold};
-    background-color: ${(props) => props.theme.color.gray200};
+  & > img {
+    width: 0.5rem;
+    height: 0.95rem;
+    position: absolute;
+    top: 1.425rem;
+    right: 0.8rem;
   }
 `;
-
-const StIconHomeCategory = styled.img`
-  width: 2.4rem;
-  height: 2.4rem;
-`;
-
-const StIconArrowBig = styled.img`
-  width: 1.4rem;
-  height: 2.8rem;
-`;
-
-const MenuCategory = styled.section``;
